c79901b71887011d354fd095c71e894fe1f175de
Andrea Mirarchi <andrea.mirarchi03@gmail.com>
2025-09-09 18:16:31 +0200

    Creazione pagina /crea

---
+++ e20-angular/src/app/components/crea/crea.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Crea } from './crea';
+
+describe('Crea', () => {
+  let component: Crea;
+  let fixture: ComponentFixture<Crea>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [Crea]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(Crea);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ e20-angular/src/app/components/crea/crea.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crea',
+  imports: [],
+  templateUrl: './crea.html',
+  styleUrl: './crea.css'
+})
+export class Crea {
+
+}
