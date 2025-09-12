d300525d45db694bd4c59189b3812b9c3360d57f
Andrea Mirarchi <andrea.mirarchi03@gmail.com>
2025-09-12 16:24:38 +0200

    Iniziata a creare la schermata del profilo e degli ordini

---
+++ e20-angular/src/app/components/profilo-e-ordini/profilo-e-ordini.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProfiloEOrdini } from './profilo-e-ordini';
+
+describe('ProfiloEOrdini', () => {
+  let component: ProfiloEOrdini;
+  let fixture: ComponentFixture<ProfiloEOrdini>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ProfiloEOrdini]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ProfiloEOrdini);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
